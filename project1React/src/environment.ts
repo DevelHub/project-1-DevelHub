d22255510828a43a90ee13f80e89a796535f19e3
Mubaraq Akanbi <Makanbi1@student.gsu.edu>
2018-09-07 15:54:42 -0400

    Update environment.ts

--- project1React/src/environment.ts
+++ project1React/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http:ec2-34-210-54-222.us-west-2.compute.amazonaws.com:8080/'
+  context: 'http:ec2-34-210-54-222.us-west-2.compute.amazonaws.com:4000/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
